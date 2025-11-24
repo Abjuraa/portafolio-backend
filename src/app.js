1cb1fac89f8246a98442211b45155c596f1a04b9
Abjura <107774277+Abjuraa@users.noreply.github.com>
2025-11-23 23:01:28 -0500

    refactor: se agregaron las rutas en el cors para poder hacer las peticiones sin que se bloqueen

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import resendRoutes from './routes/resend.routes.js'
 const app = express();
 
 app.use(cors({
+    origin: ["https://repositorio-delta-livid.vercel.app", "http://localhost:3000"],
     methods: ['POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
